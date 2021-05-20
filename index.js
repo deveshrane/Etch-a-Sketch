dd989e63fe4cedefc194550abe7c9e1924a0cf10
Devesh Rane <deveshkd73@gmail.com>
2021-05-20 16:23:45 +0530

    Project Completed

---
+++ index.js
@@ -0,0 +1,40 @@
+const container = document.querySelector('#container');
+const btn = document.querySelector('#btn');
+let val = 16;
+let r, g, b;
+function defaultBoxMaker() {
+    for (let i = 1; i <= val * val; i++) {
+        const div = document.createElement('div');
+        div.setAttribute('id', i);
+        div.classList.add("box");
+        container.appendChild(div);
+    }
+    container.style.gridTemplateColumns = `repeat(${val}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${val}, 1fr)`;
+    etchASketch();
+}
+function randColor() {
+    r = Math.floor(Math.random() * 255);
+    g = Math.floor(Math.random() * 255);
+    b = Math.floor(Math.random() * 255);
+}
+function etchASketch() {
+    randColor();
+    container.addEventListener('mouseover', function (event) {
+        event.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        container.style.tbackgroundColor = "white";
+        randColor();
+    }, false);
+}
+btn.addEventListener('click', () => {
+    for (let i = 1; i <= val * val; i++) {
+        const div = document.getElementById(i);
+        container.removeChild(div);
+    }
+    val = prompt("Enter the value of grid square you want.");
+    if (!(val >= 2 && val <= 64)) {
+        alert('Value should be from 0 to 64');
+    }
+    defaultBoxMaker();
+}, false);
+defaultBoxMaker();
